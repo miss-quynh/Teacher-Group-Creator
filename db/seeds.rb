a4ad16472e4aa5272d7119035615e2b42e5f73ae
Matthew Shepherd & Britt Flowers <pair+MatthewShepherd.beatnikflow@devbootcamp.com>
2017-11-01 15:57:11 -0700

    work on show and index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# 5.times do |i|
+#   t = Teacher.create(full_name: "teacher_#{i+1}")
+
+#   10.times do
+
+#     Student.create(teacher_id: t.id, )
+#   end
+# end
