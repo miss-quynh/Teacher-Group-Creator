ee557f50fdda6ea9922045a269007ba63088575c
Caroline & Quynh Nguyen <pair+carolyoon.miss-quynh@devbootcamp.com>
2017-11-01 15:37:49 -0700

    Split full name column into first and last name

--- db/migrate/20171101173642_create_teachers.rb
+++ db/migrate/20171101173642_create_teachers.rb
@@ -1,7 +1,8 @@
 class CreateTeachers < ActiveRecord::Migration[5.1]
   def change
     create_table :teachers do |t|
-      t.string :full_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.string :email, null: false
       t.string :password_digest
 
