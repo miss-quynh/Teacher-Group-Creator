a4398c884b028727a18a08f1ffe8a2d6fdc39138
Quynh Nguyen <qnguyenx3@gmail.com>
2017-11-02 12:24:19 -0700

    Write tests for Teacher model and pass all tests

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Teacher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:teacher) { Teacher.create(first_name: 'Mai', last_name: 'Nguyen', email: 'mai-nguyen@gmail.com', password: 'maimai') }
+  let(:students) { [Student.create(first_name: 'Quynh', last_name: 'Nguyen', grade_level: 12, gender: 'female', gpa: 3.9, teacher_id: teacher.id), Student.create(first_name: 'Huu Phu', last_name: 'Nguyen', grade_level: 9, gender: 'male', gpa: 3.48, teacher_id: teacher.id)] }
+
+  describe "associations" do
+    it "has many students" do
+      expect(teacher.students).to eq students
+    end
+  end
+
+  describe "teacher attributes" do
+    it "is valid with valid attributes" do
+      expect(teacher).to be_valid
+    end
+
+    it "is not valid without a first name" do
+      teacher.first_name = nil
+      expect(teacher).to_not be_valid
+    end
+
+    it "is not valid without a last name" do
+      teacher.last_name = nil
+      expect(teacher).to_not be_valid
+    end
+
+    it "is not valid without an email" do
+      teacher.email = nil
+      expect(teacher).to_not be_valid
+    end
+  end
+
+  describe "#full_name" do
+    it "returns the concatenated first and last name" do
+      expect(teacher.full_name).to eq 'Mai Nguyen'
+    end
+  end
+
 end
